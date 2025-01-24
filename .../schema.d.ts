40e75c76da2c9e906d0e4a36e9025f261c4c21ae
God’spower Osilama <gosi3001@7-11.com>
2025-01-24 15:11:45 -0600

    fix: unnecessary imports

--- .../schema.d.ts
+++
@@ -1,3 +0,0 @@
-export interface UtilLibGeneratorSchema {
-  name: string;
-}
--- apps/movies-api/src/main.ts
+++ apps/movies-api/src/main.ts
@@ -6,7 +6,7 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { sendNotification } from '../../../libs/api/api-util-notificationss'
+// import { sendNotification } from '../../../libs/api/api-util-notificationss'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
