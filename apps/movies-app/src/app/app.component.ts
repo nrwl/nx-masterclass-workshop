15bd24734baad5e7d900c85c8a831d07d3db0785
God’spower Osilama <gosi3001@7-11.com>
2025-01-24 14:49:00 -0600

    fix: removed faulty import

--- apps/movies-app/src/app/app.component.ts
+++ apps/movies-app/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { AppShellComponent } from './app-shell/app-shell.component';
-import { sendNotification } from '../../../../libs/api/api-util-notificationss'
+// import { sendNotification } from '../../../../libs/api/api-util-notificationss'
 
 @Component({
   standalone: true,
