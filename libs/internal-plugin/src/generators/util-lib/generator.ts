fbf856e8893898efdd34998e70a015e03747da30
nartc <nartc7789@gmail.com>
2025-01-24 09:18:23 -0600

    use js generator

--- libs/internal-plugin/src/generators/util-lib/generator.ts
+++ libs/internal-plugin/src/generators/util-lib/generator.ts
@@ -1,10 +1,5 @@
-import {
-  addProjectConfiguration,
-  formatFiles,
-  generateFiles,
-  Tree,
-} from '@nx/devkit';
-import * as path from 'path';
+import { formatFiles, Tree } from '@nx/devkit';
+import { libraryGenerator } from '@nx/js';
 import { UtilLibGeneratorSchema } from './schema';
 
 export async function utilLibGenerator(
@@ -12,13 +7,9 @@ export async function utilLibGenerator(
   options: UtilLibGeneratorSchema
 ) {
   const projectRoot = `libs/${options.name}`;
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
-  });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+
+  await libraryGenerator(tree, { directory: projectRoot });
+
   await formatFiles(tree);
 }
 
