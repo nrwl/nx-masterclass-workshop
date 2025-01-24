4ef8d32c0a5a93f28cff395774692efcc1c0f09e
God’spower Osilama <gosi3001@7-11.com>
2025-01-24 13:42:13 -0600

    chore: updated the generator to include library options

--- libs/internal-plugin/src/generators/util-lib/generator.ts
+++ libs/internal-plugin/src/generators/util-lib/generator.ts
@@ -6,20 +6,28 @@ import {
 } from '@nx/devkit';
 import * as path from 'path';
 import { UtilLibGeneratorSchema } from './schema';
+import { libraryGenerator } from "@nx/js"
 
 export async function utilLibGenerator(
   tree: Tree,
   options: UtilLibGeneratorSchema
 ) {
-  const projectRoot = `libs/${options.name}`;
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
+  console.log({options})
+  // const projectRoot = `libs/${options.name}`;
+  // addProjectConfiguration(tree, options.name, {
+  //   root: projectRoot,
+  //   projectType: 'library',
+  //   sourceRoot: `${projectRoot}/src`,
+  //   targets: {},
+  // });
+  // generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+  // await formatFiles(tree);
+
+  await libraryGenerator(tree, {
+    name: `util-${options.name}`,
+    directory: `libs/${options.directory}/util-${options.name}`,
+    tags: `${options.tags}`
   });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
-  await formatFiles(tree);
 }
 
 export default utilLibGenerator;
