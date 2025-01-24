f85a312a85ee603519e256aad97b66b64205db0c
Conrad Trost <Conrad.Trost2@7-11.com>
2025-01-24 14:29:43 -0600

    movie route titles

--- apps/movies-app/src/app/app.routes.ts
+++ apps/movies-app/src/app/app.routes.ts
@@ -5,36 +5,41 @@ export const routes: Routes = [
     path: 'list/:category',
     loadComponent: () =>
       import('@nx-workshop/movies/feature-movie-list').then(
-        m => m.MovieListPageComponent
+        (m) => m.MovieListPageComponent
       ),
+    title: 'Movies List',
   },
   {
     path: 'list/genre/:id',
     loadComponent: () =>
       import('@nx-workshop/movies/feature-movie-list').then(
-        m => m.MovieListPageComponent
+        (m) => m.MovieListPageComponent
       ),
+    title: 'Movies List',
   },
   {
     path: 'movie/:id',
     loadComponent: () =>
       import('@nx-workshop/movies/feature-movie-detail').then(
-        m => m.MovieDetailPageComponent
+        (m) => m.MovieDetailPageComponent
       ),
+    title: 'Movies List',
   },
   {
     path: 'search/:query',
     loadComponent: () =>
       import('@nx-workshop/movies/feature-movie-search').then(
-        m => m.MovieSearchPageComponent
+        (m) => m.MovieSearchPageComponent
       ),
+    title: 'Movies List',
   },
   {
     path: 'my-movies',
     loadComponent: () =>
       import('@nx-workshop/movies/feature-my-movie-list').then(
-        m => m.MyMovieListComponent
+        (m) => m.MyMovieListComponent
       ),
+    title: 'Movies List',
   },
   {
     path: '',
@@ -45,7 +50,8 @@ export const routes: Routes = [
     path: '**',
     loadComponent: () =>
       import('@nx-workshop/shared/feature-not-found-page').then(
-        m => m.NotFoundPageComponent
+        (m) => m.NotFoundPageComponent
       ),
+    title: 'Movies List',
   },
 ];
